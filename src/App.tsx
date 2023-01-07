ad8fc1f2efa7ecbdc82bf8c43319a3756e9d1f57
Jeremias Marinho <jeremias10@msn.com>
2023-01-07 15:05:27 -0300

    conclusão do global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 
 import { Button } from "./components/Button";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
@@ -13,6 +14,8 @@ export function App() {
    <Button variant="success"/>
    <Button variant="danger"/>
    <Button />
+
+   <GlobalStyle />
    </ThemeProvider>
   )
 }
