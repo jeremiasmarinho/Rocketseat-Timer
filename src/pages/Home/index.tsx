c36c5b37dc622f0a6d1772f662ad79c3f7575ecb
Jeremias Marinho <jeremias10@msn.com>
2023-01-08 21:58:37 -0300

    Finalizando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,14 +17,23 @@ export function Home() {
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
                         id="task"
+                        list="task-suggestions"
                         placeholder="Dê um nome para o seu projeto"
                     />
-
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1"></option>
+                        <option value="Projeto 2"></option>
+                        <option value="Projeto 3"></option>
+                        <option value="Projeto 4"></option>
+                    </datalist>
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesAmountInput
                         type="number"
                         id="minutesAmount"
                         placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
                     />
 
                     <span>minutos.</span>
